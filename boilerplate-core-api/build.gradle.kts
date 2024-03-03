ea4ab976071ba8c998b747f73176a8ec42372686
pius712 <aksfbsgnlfls@gmail.com>
2024-03-04 00:01:44 +0900

    Initial commit

---
+++ boilerplate-core-api/build.gradle.kts
@@ -0,0 +1,15 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+dependencies {
+    implementation(project(":storage:db-core"))
+    implementation(project(":support:logging"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+}
---
+++ boilerplate-core-api/src/test/kotlin/pius/boilerplate/KtApplicationTests.kt
@@ -0,0 +1,13 @@
+package pius.boilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
